d17810adb95da96b218ea9dccdccc5413e8e4701
mohdaamil120 <mohdaamil120@gmail.com>
2024-03-31 12:36:16 +0530

    added export data funcnality

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,6 +4,71 @@ import Sidebar from './Sidebar';
 import Page from './Page';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
+
+const HomePage = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  useEffect(() => {
+    // Function to set initial sidebar state based on screen width
+    const setInitialSidebarState = () => {
+      setShowSidebar(window.innerWidth > 580); // Set showSidebar to true if screen width is greater than 580px
+    };
+
+    setInitialSidebarState();
+
+    const handleResize = () => {
+      setShowSidebar(window.innerWidth > 580);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+ 
+
+  const exportPageConfiguration = () => {
+    const pageConfiguration = localStorage.getItem("modalData"); 
+    const json = JSON.stringify(pageConfiguration); // Convert data to JSON format
+    const blob = new Blob([json], { type: 'application/json' }); // Create a Blob object
+    const url = URL.createObjectURL(blob); // Create a URL for the Blob object
+  
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'page_configuration.json'; // Set filename for the exported JSON file
+    a.click();
+  };
+
+ 
+
+  return (
+    <Container>
+      {showSidebar ? (
+        <SidebarContainer>
+          <Sidebar />
+        </SidebarContainer>
+      ) : null}
+      <MainContainer>
+        <ToggleButton onClick={toggleSidebar}>
+          {showSidebar ? <FaTimes /> : <FaBars />}
+        </ToggleButton>
+        <Page />
+        <ExportButton onClick={exportPageConfiguration}>Export Configuration</ExportButton>
+      </MainContainer>
+    </Container>
+  );
+};
+
+export default HomePage;
+
+
+
 const Container = styled.div`
   display: flex;
   height: 100vh;
@@ -33,47 +98,18 @@ const ToggleButton = styled.button`
   z-index: 999;
 `;
 
-const HomePage = () => {
-  const [showSidebar, setShowSidebar] = useState(true);
 
-  useEffect(() => {
-    // Function to set initial sidebar state based on screen width
-    const setInitialSidebarState = () => {
-      setShowSidebar(window.innerWidth > 580); // Set showSidebar to true if screen width is greater than 580px
-    };
-
-    setInitialSidebarState();
-
-    const handleResize = () => {
-      setShowSidebar(window.innerWidth > 580);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const toggleSidebar = () => {
-    setShowSidebar(!showSidebar);
-  };
-
-  return (
-    <Container>
-      {showSidebar ? (
-        <SidebarContainer>
-          <Sidebar />
-        </SidebarContainer>
-      ) : null}
-      <MainContainer>
-        <ToggleButton onClick={toggleSidebar}>
-          {showSidebar ? <FaTimes /> : <FaBars />}
-        </ToggleButton>
-        <Page />
-      </MainContainer>
-    </Container>
-  );
-};
-
-export default HomePage;
+const ExportButton = styled.button`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-right: 20px;
+  margin-left: 20px;
+`
